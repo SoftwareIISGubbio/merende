13c6953df55838141b3c446a1abd19c64965e425
albipieretti <albipieretti@gmail.com>
2023-05-04 10:51:00 +0200

    modificate create table, insert e nomi campi

---
+++ sql/creazione.sql
@@ -0,0 +1,80 @@
+Create table classe(
+    descrizione varchar(20) primary key
+);
+
+CREATE TABLE utente(
+    cf varchar(16) PRIMARY KEY,
+    nome varchar(20),
+    cognome varchar(20),
+    utente varchar(20),
+    password varchar(20),
+    tipo ENUM ("alunno","capoclasse","gestore"),
+    fk_descrizione varchar(20),
+    FOREIGN KEY (fk_descrizione) REFERENCES classe(descrizione) on DELETE CASCADE on update CASCADE
+    );
+    
+CREATE TABLE prenotazione(
+    id_prenotazione int AUTO_INCREMENT PRIMARY KEY,
+    ricreazione varchar(20),
+    data_prenotazione date,
+    fk_cf varchar(16),
+    FOREIGN KEY (fk_cf) REFERENCES utente(cf) on DELETE CASCADE on update CASCADE
+    );
+    
+CREATE TABLE prodotto(
+    id_prodotto int AUTO_INCREMENT PRIMARY KEY,
+    prezzo double,
+    nome varchar(20),
+    calorie int,
+    grassi double,
+    zuccheri double,
+    proteine double
+    );
+    
+CREATE TABLE recensione(
+    id_recensione int AUTO_INCREMENT PRIMARY KEY,
+    voto int,
+    descrizione varchar(100),
+    fk_cf varchar(16),
+    fk_id_prodotto int,
+    FOREIGN KEY (fk_cf) REFERENCES utente(cf) on DELETE CASCADE on update CASCADE,
+    FOREIGN KEY (fk_id_prodotto) REFERENCES prodotto(id_prodotto) on DELETE CASCADE on update CASCADE
+    );
+    
+CREATE TABLE allergene(
+    id_allergene int AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(20)
+    );
+    
+CREATE TABLE promozione(
+    id_promozione int AUTO_INCREMENT PRIMARY KEY,
+    descrizione varchar(30)
+    );
+    
+CREATE TABLE prenotazione_prodotto(
+    id_prenotazione_prodotto int AUTO_INCREMENT PRIMARY KEY,
+    quantita int,
+    fk_id_prenotazione int,
+    fk_id_prodotto int,
+    FOREIGN KEY (fk_id_prenotazione) REFERENCES prenotazione (id_prenotazione)on DELETE CASCADE on update CASCADE,
+    FOREIGN KEY (fk_id_prodotto) REFERENCES prodotto(id_prodotto) on DELETE CASCADE on update CASCADE
+   );
+    
+CREATE TABLE allergene_prodotto(
+    id_allergene_prodotto int AUTO_INCREMENT PRIMARY KEY,
+    fk_id_allergene int,
+    FOREIGN KEY (fk_id_allergene) REFERENCES allergene(id_allergene) on DELETE CASCADE on update CASCADE,
+    fk_id_prodotto int,
+    FOREIGN KEY (fk_id_prodotto) REFERENCES prodotto(id_prodotto) on DELETE CASCADE on update CASCADE
+   );
+    
+CREATE TABLE promozione_prodotto(
+    id_promozione_prodotto int AUTO_INCREMENT PRIMARY KEY,
+    inzio date,
+    fine date,
+    sconto int,
+    fk_id_promozione int,
+    FOREIGN KEY (fk_id_promoziobne) REFERENCES promozione(id_promozione) on DELETE CASCADE on update CASCADE,
+    fk_id_prodotto int,
+    FOREIGN KEY (fk_id_prodotto) REFERENCES prodotto(id_prodotto) on DELETE CASCADE on update CASCADE
+   );
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,70 +1,70 @@
---INSERT TABELLA CLASSI
+-- INSERT TABELLA CLASSI
 INSERT INTO classe (descrizione) VALUES ('5i');
 INSERT INTO classe (descrizione) VALUES ('4i');
 INSERT INTO classe (descrizione) VALUES ('3i');
 INSERT INTO classe (descrizione) VALUES ('bar');
 INSERT INTO classe (descrizione) VALUES ('2i');
 
---INSERT TABELLA UTENTI
-INSERT INTO utenti (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('PRTLRT', 'Alberto', 'Pieretti', 'albipieretti', 'ciao123', 'capoclasse', '5i');
-INSERT INTO utenti (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('SRNSML', 'Samuel', 'Soriani', 'samufrix', 'bella', 'studente', '5i');
-INSERT INTO utenti (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('SRNLCA', 'Luca', 'Soriani', 'luchino10', 'AUAU', 'studente', '3i');
-INSERT INTO utenti (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('GLLLCU', 'Luca', 'Galletti', 'gallo', 'cantiano', 'gestore', 'bar');
-INSERT INTO utenti (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('MLTLND', 'Leonardo', 'Meletti', 'leomela04', 'M3L4', 'studente', '2i');
+-- INSERT TABELLA UTENTI
+INSERT INTO utente (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('PRTLRT', 'Alberto', 'Pieretti', 'albipieretti', 'ciao123', 'capoclasse', '5i');
+INSERT INTO utente (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('SRNSML', 'Samuel', 'Soriani', 'samufrix', 'bella', 'alunno', '5i');
+INSERT INTO utente (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('SRNLCA', 'Luca', 'Soriani', 'luchino10', 'AUAU', 'alunno', '3i');
+INSERT INTO utente (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('GLLLCU', 'Luca', 'Galletti', 'gallo', 'cantiano', 'gestore', 'bar');
+INSERT INTO utente (cf, nome, cognome, utente, password, tipo, fk_descrizione) VALUES ('MLTLND', 'Leonardo', 'Meletti', 'leomela04', 'M3L4', 'alunno', '2i');
 
---INSERT TABELLA PRENOTAZIONI
-INSERT INTO prenotazione (id_pre, ricreazione, data_pre, fk_cf) VALUES (null, '1', '2023-03-21', 'PRTLRT');
-INSERT INTO prenotazione (id_pre, ricreazione, data_pre, fk_cf) VALUES (null, '1', '2023-04-01', 'SRNSML');
-INSERT INTO prenotazione (id_pre, ricreazione, data_pre, fk_cf) VALUES (null, '2', '2023-01-15', 'PRTLRT');
-INSERT INTO prenotazione (id_pre, ricreazione, data_pre, fk_cf) VALUES (null, '1', '2023-05-05', 'SRNLCA');
-INSERT INTO prenotazione (id_pre, ricreazione, data_pre, fk_cf) VALUES (null, '2', '2023-05-05', 'GLLLCU');
+-- INSERT TABELLA PRENOTAZIONI
+INSERT INTO prenotazione (id_prenotazione, ricreazione, data_prenotazione, fk_cf) VALUES (null, '1', '2023-03-21', 'PRTLRT');
+INSERT INTO prenotazione (id_prenotazione, ricreazione, data_prenotazione, fk_cf) VALUES (null, '1', '2023-04-01', 'SRNSML');
+INSERT INTO prenotazione (id_prenotazione, ricreazione, data_prenotazione, fk_cf) VALUES (null, '2', '2023-01-15', 'PRTLRT');
+INSERT INTO prenotazione (id_prenotazione, ricreazione, data_prenotazione, fk_cf) VALUES (null, '1', '2023-05-05', 'SRNLCA');
+INSERT INTO prenotazione (id_prenotazione, ricreazione, data_prenotazione, fk_cf) VALUES (null, '2', '2023-05-05', 'GLLLCU');
 
---INSERT TABELLA PRODOTTI
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto', 120, 30.56, 70.5, 20.5);
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto', 120, 30.56, 70.5, 20.5);
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 1.2, 'cocacola', 30, 120.45, 78, 45);
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino salame', 45, 80, 20, 40.9);
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto co', 45, 50, 46, 10.06);
-INSERT INTO prodotto (id_prod, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 1.75, 'cornetto cioccolato', 67, 70, 200, 22);
+-- INSERT TABELLA PRODOTTI
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto', 120, 30.56, 70.5, 20.5);
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto', 120, 30.56, 70.5, 20.5);
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 1.2, 'cocacola', 30, 120.45, 78, 45);
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino salame', 45, 80, 20, 40.9);
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 2, 'panino prosciutto co', 45, 50, 46, 10.06);
+INSERT INTO prodotto (id_prodotto, prezzo, nome, calorie, grassi, zuccheri, proteine) VALUES (null, 1.75, 'cornetto cioccolato', 67, 70, 200, 22);
 
---INSERT TABELLA RECENSIONI
-INSERT INTO recensione (id_rec, voto, descrizione, fk_cf, fk_id_prod) VALUES (null, 5, 'ottimo panino', 'PRTLRT', 1);
-INSERT INTO recensione (id_rec, voto, descrizione, fk_cf, fk_id_prod) VALUES (null, 1, 'panino ammuffito', 'GLLLCU', 1);
-INSERT INTO recensione (id_rec, voto, descrizione, fk_cf, fk_id_prod) VALUES (null, 3, 'cocacola non freschissima', 'PRTLRT', 3);
-INSERT INTO recensione (id_rec, voto, descrizione, fk_cf, fk_id_prod) VALUES (null, 5, 'cocacola fresca', 'MLTLND', 3);
-INSERT INTO recensione (id_rec, voto, descrizione, fk_cf, fk_id_prod) VALUES (null, 2, 'cornetto ghiaccio', 'SRNLCA', 5);
+-- INSERT TABELLA RECENSIONI
+INSERT INTO recensione (id_recensione, voto, descrizione, fk_cf, fk_id_prodotto) VALUES (null, 5, 'ottimo panino', 'PRTLRT', 1);
+INSERT INTO recensione (id_recensione, voto, descrizione, fk_cf, fk_id_prodotto) VALUES (null, 1, 'panino ammuffito', 'GLLLCU', 1);
+INSERT INTO recensione (id_recensione, voto, descrizione, fk_cf, fk_id_prodotto) VALUES (null, 3, 'cocacola non freschissima', 'PRTLRT', 3);
+INSERT INTO recensione (id_recensione, voto, descrizione, fk_cf, fk_id_prodotto) VALUES (null, 5, 'cocacola fresca', 'MLTLND', 3);
+INSERT INTO recensione (id_recensione, voto, descrizione, fk_cf, fk_id_prodotto) VALUES (null, 2, 'cornetto ghiaccio', 'SRNLCA', 5);
 
---INSERT TABELLA PROMOZIONI
-INSERT INTO promozione (id_pro, descrizione) VALUES (null, 'promozione natale');
-INSERT INTO promozione (id_pro, descrizione) VALUES (null, 'promozione pasqua');
-INSERT INTO promozione (id_pro, descrizione) VALUES (null, 'sconti ceri');
-INSERT INTO promozione (id_pro, descrizione) VALUES (null, 'sconti hallowen');
-INSERT INTO promozione (id_pro, descrizione) VALUES (null, 'promozione lunedi');
+-- INSERT TABELLA PROMOZIONI
+INSERT INTO promozione (id_promozione, descrizione) VALUES (null, 'promozione natale');
+INSERT INTO promozione (id_promozione, descrizione) VALUES (null, 'promozione pasqua');
+INSERT INTO promozione (id_promozione, descrizione) VALUES (null, 'sconti ceri');
+INSERT INTO promozione (id_promozione, descrizione) VALUES (null, 'sconti hallowen');
+INSERT INTO promozione (id_promozione, descrizione) VALUES (null, 'promozione lunedi');
 
---INSERT TABELLA ALLERGENI
-INSERT INTO allergene (id_al, nome) VALUES (null, 'glutine');
-INSERT INTO allergene (id_al, nome) VALUES (null, 'lattosio');
-INSERT INTO allergene (id_al, nome) VALUES (null, 'arachidi');
-INSERT INTO allergene (id_al, nome) VALUES (null, 'soia');
-INSERT INTO allergene (id_al, nome) VALUES (null, 'sesamo');
+-- INSERT TABELLA ALLERGENI
+INSERT INTO allergene (id_allergene, nome) VALUES (null, 'glutine');
+INSERT INTO allergene (id_allergene, nome) VALUES (null, 'lattosio');
+INSERT INTO allergene (id_allergene, nome) VALUES (null, 'arachidi');
+INSERT INTO allergene (id_allergene, nome) VALUES (null, 'soia');
+INSERT INTO allergene (id_allergene, nome) VALUES (null, 'sesamo');
 
---INSERT TABELLA ALLERGENI_PRODOTTI
-INSERT INTO allergene_prodotto(id_ap, fk_id_al, fk_id_prod) VALUES (null, 1, 1);
-INSERT INTO allergene_prodotto(id_ap, fk_id_al, fk_id_prod) VALUES (null, 1, 4);
-INSERT INTO allergene_prodotto(id_ap, fk_id_al, fk_id_prod) VALUES (null, 3, 2);
-INSERT INTO allergene_prodotto(id_ap, fk_id_al, fk_id_prod) VALUES (null, 5, 1);
-INSERT INTO allergene_prodotto(id_ap, fk_id_al, fk_id_prod) VALUES (null, 2, 2);
+-- INSERT TABELLA ALLERGENI_PRODOTTI
+INSERT INTO allergene_prodotto(id_allergene_prodotto, fk_id_allergene, fk_id_prodotto) VALUES (null, 1, 1);
+INSERT INTO allergene_prodotto(id_allergene_prodotto, fk_id_allergene, fk_id_prodotto) VALUES (null, 4, 4);
+INSERT INTO allergene_prodotto(id_allergene_prodotto, fk_id_allergene, fk_id_prodotto) VALUES (null, 3, 2);
+INSERT INTO allergene_prodotto(id_allergene_prodotto, fk_id_allergene, fk_id_prodotto) VALUES (null, 5, 1);
+INSERT INTO allergene_prodotto(id_allergene_prodotto, fk_id_allergene, fk_id_prodotto) VALUES (null, 2, 2);
 
---INSERT TEBALLA PRENOTAZIONI_PRODOTTI
-INSERT INTO prenotazione_prodotto(id_pp, quantita, fk_id_pre, fk_id_prod) VALUES (null, 2, 1, 1);
-INSERT INTO prenotazione_prodotto(id_pp, quantita, fk_id_pre, fk_id_prod) VALUES (null, 1, 2, 1);
-INSERT INTO prenotazione_prodotto(id_pp, quantita, fk_id_pre, fk_id_prod) VALUES (null, 3, 2, 3);
-INSERT INTO prenotazione_prodotto(id_pp, quantita, fk_id_pre, fk_id_prod) VALUES (null, 1, 4, 5);
-INSERT INTO prenotazione_prodotto(id_pp, quantita, fk_id_pre, fk_id_prod) VALUES (null, 1, 3, 3);
+-- INSERT TEBALLA PRENOTAZIONI_PRODOTTI
+INSERT INTO prenotazione_prodotto(id_prenotazione_prodotto, quantita, fk_id_prenotazione, fk_id_prodotto) VALUES (null, 2, 1, 1);
+INSERT INTO prenotazione_prodotto(id_prenotazione_prodotto, quantita, fk_id_prenotazione, fk_id_prodotto) VALUES (null, 1, 2, 1);
+INSERT INTO prenotazione_prodotto(id_prenotazione_prodotto, quantita, fk_id_prenotazione, fk_id_prodotto) VALUES (null, 3, 5, 3);
+INSERT INTO prenotazione_prodotto(id_prenotazione_prodotto, quantita, fk_id_prenotazione, fk_id_prodotto) VALUES (null, 1, 4, 5);
+INSERT INTO prenotazione_prodotto(id_prenotazione_prodotto, quantita, fk_id_prenotazione, fk_id_prodotto) VALUES (null, 1, 3, 3);
 
---INSERT TABELLA PROMOZIONI_PRODOTTI
-INSERT INTO promozione_prodotto (id_sc, inzio, fine, sconto, fk_id_pro, fk_id_prod) VALUES (1, '2022-12-23', '2023-01-09', 30, 1, 1);
-INSERT INTO promozione_prodotto (id_sc, inzio, fine, sconto, fk_id_pro, fk_id_prod) VALUES (2, '2023-01-09', '2023-01-09', 20, 2, 5);
-INSERT INTO promozione_prodotto (id_sc, inzio, fine, sconto, fk_id_pro, fk_id_prod) VALUES (3, '2023-04-09', '2023-04-16', 25, 5, 2);
-INSERT INTO promozione_prodotto (id_sc, inzio, fine, sconto, fk_id_pro, fk_id_prod) VALUES (4, '0000-00-00', '2022-07-11', 30, 4, 2);
-INSERT INTO promozione_prodotto (id_sc, inzio, fine, sconto, fk_id_pro, fk_id_prod) VALUES (5, '2022-06-15', '2022-06-21', 30, 3, 4);
+-- INSERT TABELLA PROMOZIONI_PRODOTTO
+INSERT INTO promozione_prodotto (id_promozione_prodotto, inzio, fine, sconto, fk_id_promozione, fk_id_prodotto) VALUES (1, '2022-12-23', '2023-01-09', 30, 1, 1);
+INSERT INTO promozione_prodotto (id_promozione_prodotto, inzio, fine, sconto, fk_id_promozione, fk_id_prodotto) VALUES (2, '2023-01-09', '2023-01-09', 20, 2, 5);
+INSERT INTO promozione_prodotto (id_promozione_prodotto, inzio, fine, sconto, fk_id_promozione, fk_id_prodotto) VALUES (3, '2023-04-09', '2023-04-16', 25, 5, 2);
+INSERT INTO promozione_prodotto (id_promozione_prodotto, inzio, fine, sconto, fk_id_promozione, fk_id_prodotto) VALUES (4, '0000-00-00', '2022-07-11', 30, 4, 2);
+INSERT INTO promozione_prodotto (id_promozione_prodotto, inzio, fine, sconto, fk_id_promozione, fk_id_prodotto) VALUES (5, '2022-06-15', '2022-06-21', 30, 3, 4);
