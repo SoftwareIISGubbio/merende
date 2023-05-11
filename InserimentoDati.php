91c34c9cdb4273d4a2053f59287b1b191c09497e
Roberto Matteucci <robimatte04@gmail.com>
2023-05-11 11:45:23 +0200

    aggiunto al lavore 11/05/23

--- InserimentoDati.php
+++ InserimentoDati.php
@@ -6,14 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    textarea {
+  text-align: left;
+}
+</style>
 <body>
     <?php
-        //print_r($_POST);
-        $servername = "localhost";
-        $user = "root";
-        $pwd = "root";
+        $servername = "10.1.0.52";
+        $user = "progetto5i";
+        $pwd = "progetto5i";
         $db = "merende";
-
         $connessione = mysqli_connect($servername,$user,$pwd,$db);
 
         //controllo la connessione 
@@ -23,60 +26,87 @@
 
         if(!isset($_POST['invia'])){
     ?>
-    
-    <form method = "POST" action="inserimentoDati.php">
-            CIBO: <select name="prodotto">
+    <button value="aggiungi"  onclick ="aggiungi()">Aggiungi</button>
+    <form method = "POST" action="Dati.php">
+            CIBO: <select name="prodotto" id='prodotto'>
 
-    <?php 
-        $query = "SELECT prodotto.nome FROM  prodotto";
+    <?php
+        $prodotto;
+        $query = "SELECT * FROM  prodotto";
         $risultato = mysqli_query($connessione,$query);
 
         if(mysqli_num_rows($risultato)>0){
 
             while($riga = mysqli_fetch_array($risultato)){
-                echo ("<option value=${riga['id_prod']}>${riga['nome']}</option>");
+                echo ($riga);
+                echo ("<option  value=\"${riga['id_prodotto']}\">
+                        ${riga['nome']}</option>");
             }
         }
-
         
     ?>
         </select>
-        <input type="submit" value="aggiungi">
         <br><br>
         RICREAZIONE:
         <br><br>
         1: <input type="radio" name="ricreazione" value="1">
         2: <input type="radio" name="ricreazione" value="2">
+        
         <br><br>
         <input type="submit" value="invia">
         <input type="reset" value="cancella">
     </form>
+    
+    <textarea id="inserimento" name="area" rows="4" cols="50" EMPTY readonly></textarea>
+    <textarea id="invioNumeri" name="area" rows="4" cols="50" EMPTY readonly></textarea>
 <?php
 }else{
+    //quando pigio invio devo fare questa cosa
 
-    $id_prod = $_POST['id_prod'];
+    $id_prenotazione= $_POST['id_pronotazione'];
     $ricreazione = $_POST['ricreazione'];
-    $data = $_POST['data'];
+    $data = $_POST['data_prenotazione'];
     $cf = $_POST['fk_cf'];
 
 
-    $inserisciDati = "INSERT INTO prenotazione (id_pre,ricreazione,data_pre,fk_cf)
-     VALUES ($id_prod,$ricreazione,$data,$cf)";
+    $inserisciDati = "INSERT INTO prenotazione (id_prenotazione,ricreazione,data_pre,cf)
+     VALUES ($id_prenotazione,$ricreazione,$data,$cf)";
     echo ("prima: ".$inserisciDati);
 
     $risultato1 = mysqli_query($connessione,$inserisciDati);
     if ($risultato1){
         echo ("dati inseriti");
+        print_r($_POST);
     }else{
         echo("c'è un errore");
+        print_r($_POST);
     }
+    
     mysqli_close($connessione);
 }
 
-/*if(!isset($_POST['aggiungi'])){
-    echo();
-}*/
+
 ?>
+<script>
+        "use strict";
+        function aggiungi(){
+            let prodotto = document.getElementById("prodotto").value;
+            let nome=  document.getElementById("prodotto").selectedIndex;
+
+            let area = document.getElementById ("inserimento");
+            let areaNumeri = document.getElementById ("invioNumeri");
+
+            let x = document.querySelectorAll("#prodotto option");
+            console.log(nome, prodotto);
+            areaNumeri.value += prodotto;
+            area.value +=x[prodotto-1].innerText;
+
+            
+            console.log(x[prodotto-1].innerText);
+           
+        }
+    </script>
+
 </body>
 </html>
 
